18f31e4b8b2d70fcfafa32fbdb34b6f8b2fb0b8b
goatmark <102983476+goatmark@users.noreply.github.com>
2024-11-15 20:39:20 +0000

    initial openai implementation and test prompts

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -14,12 +14,25 @@ class MainController < ApplicationController
 
   def processing
     prompt_1 = "On Tuesday, I need to pick up my dry cleaning."
-    prompt_2 = "Please make a note, today is November 15th, 2024, and I am just beginning my final project for Application Development."
-    prompt_3 = "Please create a note, I have just been thinking about how sad the weather is, it reminds me of London where it is cold and it is gray and it is sad."
+    prompt_2 = "I have an assignment for my Digital Marketing Lab class due on Saturday."
+    prompt_3 = "I need to send a follow up email to Jessica about the cannabis club."
+    prompt_4 = "I need to go to the banquet next month."
+    prompt_5 = "AYO GPT - can you help a m*f* remember that he's gotta fill up a m*f* car with some m*f* gas by Monday?"
+
+    @chosen_prompt = prompt_5
 
     openai_class = OpenaiService.new
 
-    @result = openai_class.prompt_classify(message: prompt_1)
+    @result = openai_class.prompt_classify(message: @chosen_prompt)
+
+    if @result == "note"
+      @body = openai_class.prompt_note_summary(message: @chosen_prompt)
+    elsif @result == "task"
+      @task = openai_class.prompt_extract_task(message: @chosen_prompt)
+      @deadline = openai_class.prompt_extract_deadline(message: @chosen_prompt)
+    else
+    end
+    
     render({:template => "main_templates/processing"})
   end
 end
--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -5,12 +5,12 @@ class OpenaiService
     @client = OpenAI::Client.new
   end
 
-  def prompt_classify(message: "", chosen_model:  "gpt-4o")
+  def prompt_classify(message: "", chosen_model:  "gpt-4o-mini")
     response = @client.chat(
       parameters: {
-        model: chosen_model, # or another model like "gpt-3.5-turbo"
+        model: chosen_model, 
         messages: [
-          { role: "system", content: "Return only 'note' or 'task' without quotes to best classify the message. Never respond otherwise." },
+          { role: "system", content: "Return only 'note' or 'task' without quotes to best classify the message (reminders are tasks). Never respond otherwise."},
           { role: "user", content: message }
         ],
         temperature: 0 # Setting temperature to 0 for deterministic output
@@ -20,5 +20,69 @@ class OpenaiService
     # Extract the response text
     classification = response.dig("choices", 0, "message", "content").strip.downcase
     return(classification)
+  end
+
+  def prompt_extract_task(message: "", chosen_model:  "gpt-4o-mini")
+    response = @client.chat(
+      parameters: {
+        model: chosen_model, 
+        messages: [
+          { role: "system", content: "Extract only a summary of the task as if it were to be put on a To-Do list or Kanban board. 
+          Do not use any leading bullet/hyphen characters.
+          If a deadline is specified, do not include it in the task summary; it will be extracted separately."},
+          { role: "user", content: message }
+        ],
+        temperature: 0.7
+      }
+    )
+
+    # Extract the response text
+    task = response.dig("choices", 0, "message", "content").strip
+    return(task)
+  end
+
+  def prompt_extract_deadline(message: "", chosen_model:  "gpt-4o-mini")
+    today = Date.today
+    tomorrow = today + 1
+    dow = today.strftime('%A')
+    todays_date=today.strftime('%Y-%m-%d')
+    tomorrows_date=tomorrow.strftime('%Y-%m-%d')
+    response = @client.chat(
+      parameters: {
+        model: chosen_model, 
+        messages: [
+          { role: "system", content: "Return only the deadline knowing today is #{dow}, #{todays_date}.
+          (e.g. if something says it's due Tuesday, assume it's due the first Tuesday after #{todays_date}).
+          If no deadline specified, return #{tomorrows_date}. 
+          If something is due 'next week', assume a 7 day deadline from today. 
+          If something is due 'next month', assume a deadline a full calendar month from today.
+          Strictly always respond in  'YYYY-MM-DD' format without quotes."},
+          { role: "user", content: message }
+        ],
+        temperature: 0.7
+      }
+    )
+    # Extract the response text
+    deadline = response.dig("choices", 0, "message", "content").strip
+    return(deadline)
+  end
+
+  def prompt_note_summary(message: "", chosen_model:  "gpt-4o-mini")
+    response = @client.chat(
+      parameters: {
+        model: chosen_model, 
+        messages: [
+          { role: "system", content: "Context: You are responsible for parsing elements of a dictation.
+          Your job is extract just the 'note' part of the message given to you, excluding any commands given to the dictation app.
+          In other words, if receiving something that says 'Make a note that I need to...', return 'I need to...' onwards.
+          Return the full note. Do NOT summarize, paraphrase, or copy edit. You may only make minor tweaks to address filler words (ums/likes), misspoken words, or fractured sentences. Do NOT summarize. Do NOT attempt to improve quality of writing beyond specification."},
+          { role: "user", content: message }
+        ],
+        temperature: 0.7
+      }
+    )
+    # Extract the response text
+    body = response.dig("choices", 0, "message", "content").strip
+    return(body)
   end
 end
